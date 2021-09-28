fa724f82cd2a2e2b0dfb837bb9b0e880f074ceb5
Евгений Бухаров <evgeny@MacBook-Pro-Evgenij.local>
2021-09-29 00:47:18 +0300

    tadd tests for posts

---
+++ test/controllers/posts_controller_test.rb
@@ -0,0 +1,65 @@
+require "test_helper"
+
+class PostsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @post = posts(:one)
+    @user = users(:one)
+    @post_category = post_categories(:one)
+    sign_in @user
+  end
+
+  test "should get index" do
+    get posts_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_post_url
+    assert_response :success
+  end
+
+  test "should create post" do
+    post = {
+      title: 'string',
+      body: 'text',
+      post_category_id: @post_category.id
+    }
+
+    assert_difference('Post.count') do
+      post posts_url, params: { post: post }
+    end
+
+    assert_redirected_to post_url(Post.last)
+  end
+
+  test "should show post" do
+    get post_url(@post)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_post_url(@post)
+    assert_response :success
+  end
+
+  test "should update post" do
+    post = {
+      title: 'string',
+      body: 'new_text',
+      post_category_id: @post_category.id
+    }
+
+    patch post_url(@post), params: { post: post }
+    assert_redirected_to post_url(@post)
+  end
+
+  test "should destroy post" do
+    assert_difference('Post.count', -1) do
+      delete post_url(@post)
+    end
+
+    assert_redirected_to posts_url
+  end
+end
