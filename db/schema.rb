5fe00c56aa1cc760f0b0fad27199a9062eb2a9ac
Евгений Бухаров <evgeny@MacBook-Pro-Evgenij.local>
2021-10-01 00:19:59 +0300

    add comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_28_194424) do
+ActiveRecord::Schema.define(version: 2021_09_30_202432) do
 
   create_table "post_categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
